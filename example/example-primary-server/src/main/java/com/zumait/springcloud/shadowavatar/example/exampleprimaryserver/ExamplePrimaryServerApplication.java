65f5199d07b92fb9d8d0923eb3d0ca44d7f76c61
nicholaszhu <nichols.zu@gmail.com>
2019-12-08 21:55:45 +1100

    create example project

---
+++ example/example-primary-server/src/main/java/com/zumait/springcloud/shadowavatar/example/exampleprimaryserver/ExamplePrimaryServerApplication.java
@@ -0,0 +1,13 @@
+package com.zumait.springcloud.shadowavatar.example.exampleprimaryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamplePrimaryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamplePrimaryServerApplication.class, args);
+	}
+
+}
